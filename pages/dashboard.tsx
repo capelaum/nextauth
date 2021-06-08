46f7361a1703b35c41e0bd8daba902de4b5abae1
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-08 20:46:16 -0300

    initial project

---
+++ pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
+
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get("/me").then(response => console.log(response));
+  }, []);
+
+  return <h1>DashBoard: {user?.email}</h1>;
+}
