c7e24008882cfcb8e67adac034592ee39700dcfa
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-11 14:08:48 -0300

    Validate permission in server side

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -5,21 +5,53 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type withSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
 
 // redireciona para o login caso o token não esteja setado, na page dashboard
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: withSSRAuthOptions
+) {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies["nextauth.token"];
 
-    if (!cookies["nextauth.token"]) {
+    if (!token) {
       return {
         redirect: {
           destination: "/",
           permanent: false,
         },
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      console.log("🚀 ~ user", user);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
     }
 
     try {
